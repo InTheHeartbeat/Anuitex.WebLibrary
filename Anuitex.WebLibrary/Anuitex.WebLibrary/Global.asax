f7f5bf7ec87dead98a257ebca6ade1529ff2ed75
InTheHeartbeat <intheheartbeat0@gmail.com>
2017-08-28 01:17:53 +0300

    Project initialized

---
+++ Anuitex.WebLibrary/Anuitex.WebLibrary/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Anuitex.WebLibrary.MvcApplication" Language="C#" %>
---
+++ Anuitex.WebLibrary/Anuitex.WebLibrary/Scripts/Site/interactive.js
@@ -0,0 +1,21 @@
+﻿function ShowAddSchemeWindow() {
+    $("#main-wc").animate({
+            top: "100vh"
+        },
+        200,
+        function () {
+            $("#main-wc").hide();
+            $("#add-scheme-wc").show();
+            $("#add-scheme-wc").animate({
+                    left: "0"
+                },
+                200);
+        });
+}
+
+$(document).ready(function () {
+
+    $("#btn-add-scheme").click(function() {
+        ShowAddSchemeWindow();
+    });
+});
